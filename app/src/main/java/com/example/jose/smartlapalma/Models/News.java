ec2c45c96d9545c44dd3a74b338fd9bafe78972f
José Ángel Concepción Sánchez <alu0100697414@ull.edu.es>
2018-04-19 20:35:20 +0100

    Added New and News models.

---
+++ app/src/main/java/com/example/jose/smartlapalma/Models/News.java
@@ -0,0 +1,16 @@
+package com.example.jose.smartlapalma.Models;
+
+import java.util.List;
+
+public class News {
+
+    private List<New> mNewList;
+
+    public void setmNewList(List<New> mNewList) {
+        this.mNewList = mNewList;
+    }
+
+    public List<New> getmNewList() {
+        return mNewList;
+    }
+}

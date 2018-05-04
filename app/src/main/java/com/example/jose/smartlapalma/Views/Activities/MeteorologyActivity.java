57affdd51c81cffe983704d40c2cac631680ad18
José Ángel Concepción Sánchez <alu0100697414@ull.edu.es>
2018-05-04 19:59:26 +0100

    Now today meteo prediction is loaded in layout.

--- app/src/main/java/com/example/jose/smartlapalma/Views/Activities/MeteorologyActivity.java
+++ app/src/main/java/com/example/jose/smartlapalma/Views/Activities/MeteorologyActivity.java
@@ -7,9 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.jose.smartlapalma.Controllers.Utils.CustomMaterialDrawer;
 import com.example.jose.smartlapalma.Models.About;
+import com.example.jose.smartlapalma.Models.Meteorology.DayWeather;
+import com.example.jose.smartlapalma.Models.OpenDataLaPalma;
 import com.example.jose.smartlapalma.Models.SharedPreferencesKeys;
 import com.example.jose.smartlapalma.Models.UserType;
 import com.example.jose.smartlapalma.R;
@@ -43,6 +46,55 @@ public class MeteorologyActivity extends AppCompatActivity {
             customMaterialDrawer.setResidentMode();
         } else {
             customMaterialDrawer.setTouristMode();
+        }
+
+        setDataInActivity();
+    }
+
+    private void setDataInActivity(){
+
+        // Load weather information
+        OpenDataLaPalma openDataLaPalma = OpenDataLaPalma.getInstance();
+
+        if(!openDataLaPalma.getmWeather().getmDayWeatherList().isEmpty()){
+
+            // Set today data (DayWeather pos: 0)
+            DayWeather today = openDataLaPalma.getmWeather().getmDayWeatherList().get(0);
+
+            // Set max and min degrees
+            TextView todayMaxDegrees = findViewById(R.id.today_max_degrees);
+            TextView todayMinDegrees = findViewById(R.id.today_min_degrees);
+
+            todayMaxDegrees.setText(today.getmTemperature().getmMax() + getString(R.string.degree));
+            todayMinDegrees.setText(today.getmTemperature().getmMin() + getString(R.string.degree));
+
+            // Set UV
+            TextView todayUV = findViewById(R.id.uv_value);
+            todayUV.setText(today.getmUV().getmUV());
+
+            // Set description
+            TextView todayDescription = findViewById(R.id.today_description);
+            todayDescription.setText(today.getmSkyState().getmDescription());
+
+            // Set rain probability, wind, thermal sensation and humidity
+            TextView todayRainProbability = findViewById(R.id.today_rain_probability);
+            todayRainProbability.setText(today.getmPrecipitation().getmPrecipitationValue() +
+                    getString(R.string.percentage));
+
+            TextView todayWind = findViewById(R.id.today_wind);
+            todayWind.setText(today.getmWind().getmVelocity() + getString(R.string.velocity));
+
+            TextView todayThermalSensation = findViewById(R.id.today_thermal_sensation);
+            todayThermalSensation.setText(today.getmThermalSensation().getmMax() +
+                    getString(R.string.degree));
+
+            TextView todayHumidity = findViewById(R.id.today_humidity);
+            todayHumidity.setText(today.getmHumidity().getmMax() +
+                    getString(R.string.percentage));
+        }
+
+        else {
+            // TODO: SHOW LOAGING ERROR MESSAGE
         }
     }
 
